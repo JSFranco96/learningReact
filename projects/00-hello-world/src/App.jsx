52607f1fb990c7394d28abb69b22119da1306c47
JSFranco96 <javierfranco0904@gmail.com>
2023-09-29 11:56:35 -0500

    feat: :construction: List rendering

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -1,24 +1,59 @@
 import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+import { TwitterFollowCard } from './TwitterFollowCard';
+
+const users = [
+    {
+        name: 'Javier Steven Franco Ospina',
+        userName: 'jsfranco96',
+        isFollowing: true,
+        title: 'Fullstack developer'
+    },
+    {
+        name: 'Miguel Ángel Durán',
+        userName: 'midudev',
+        isFollowing: false,
+        title: 'Fullstack developer and best teacher ever'
+    },
+    {
+        name: 'Laura Sosa García',
+        userName: 'LAURASOSAGARCI1',
+        isFollowing: true,
+        title: 'Wife'
+    },
+    {
+        name: 'Diana Franco',
+        userName: '18_nanitha',
+        isFollowing: true,
+        title: 'Sister'
+    },
+    {
+        name: 'Aider Alexis Agudelo García',
+        userName: 'alexis9706',
+        isFollowing: false,
+        title: 'Friend'
+    }
+];
 
 export function App() {
     const formatUserName = (userName) => `@${userName}`
     return (
         <section className='App'>
-            <TwitterFollowCard
-                formatUserName={formatUserName}
-                name='Javier Steven Franco Ospina'
-                userName='jsfranco96'
-                initialIsFollowing>
-                firstElementChildren
-            </TwitterFollowCard>
+            {
+                users.map(({ userName, name, title, isFollowing }) => {
+                    return (
 
-            <TwitterFollowCard
-                formatUserName={formatUserName}
-                name='Miguel Ángel Durán'
-                userName='midudev'>
-                secondElementChildren
-            </TwitterFollowCard>
+                        <TwitterFollowCard
+                            formatUserName={formatUserName}
+                            name={name}
+                            initialIsFollowing={isFollowing}
+                            userName={userName}
+                            key={userName}>
+
+                            {title}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
